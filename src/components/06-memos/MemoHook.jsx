1509b227a069d344e749e8fff2b2c2ea5782cf10
BetoSandoval <alberto.sa1391@gmail.com>
2021-09-02 14:09:33 -0500

    memoHook procesos peados

--- src/components/06-memos/MemoHook.jsx
+++ src/components/06-memos/MemoHook.jsx
@@ -1,17 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
 import '../02-useEffect/effects.css';
-import useCounter from '../../hooks/useCounter'
+import useCounter from '../../hooks/useCounter';
+import { procesoPesado } from'../../helpers/ProcesosPesados'
 
 export default function MemoHook() {
 
-    const { counter, increment } = useCounter( 10 );
+    const { counter, increment } = useCounter( 1000 );
     const [ show, setShow ] = useState( true );
+
+    const memoProcesoPesado = useMemo( () => procesoPesado( counter ), [ counter ] );
 
     return (
         <div>
             <h1>Memo hook</h1>
-            <h3>Counter: <small> value={counter} </small></h3>
+            <h3>Counter: <small> value = {counter} </small></h3>
             <hr />
+
+            <p> { memoProcesoPesado } </p>
 
             <button className="btn btn-primary" onClick={increment}>
                 +1
